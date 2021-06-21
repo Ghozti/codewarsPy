8a94bbd413bc29291ca3688fdc9b397ceb568015
Ghozti <ybalderas1005@gmail.com>
2021-06-21 16:10:32 -0400

    - fiished BMI calc

--- KYU8/kyu8.py
+++ KYU8/kyu8.py
@@ -3,3 +3,15 @@ class NumToStr:
         return str(num)
 
 class CalculateBMI:
+    def calculate_bmi(self,w,h):
+        bmi = w / h ** 2
+
+        if bmi <= 18.5 :
+            return "underweight"
+        elif bmi <= 25.0:
+            return "normal weight"
+        elif bmi <= 30:
+            return "overweight"
+        else:
+            return "obese"
+
