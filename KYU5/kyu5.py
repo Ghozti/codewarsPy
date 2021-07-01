3a66e3c2bb459680890c2e8ee739a7c17688422e
Ghozti <ybalderas1005@gmail.com>
2021-07-01 14:26:43 -0400

    - finished ROT13

--- KYU5/kyu5.py
+++ KYU5/kyu5.py
@@ -15,9 +15,68 @@ class FindDomain:
 
         return dom
 
+
 class ROT13:
-    def rot13(self,message):
+    def rot13(self, message):
         txt = ""
+        word_dic = {
+            "a": "n",
+            "b": "o",
+            "c": "p",
+            "d": "q",
+            "e": "r",
+            "f": "s",
+            "g": "t",
+            "h": "u",
+            "i": "v",
+            "j": "w",
+            "k": "x",
+            "l": "y",
+            "m": "z",
+            "n": "a",
+            "o": "b",
+            "p": "c",
+            "q": "d",
+            "r": "e",
+            "s": "f",
+            "t": "g",
+            "u": "h",
+            "v": "i",
+            "w": "j",
+            "x": "k",
+            "y": "l",
+            "z": "m",
+
+            "A": "N",
+            "B": "O",
+            "C": "P",
+            "D": "Q",
+            "E": "R",
+            "F": "S",
+            "G": "T",
+            "H": "U",
+            "I": "V",
+            "J": "W",
+            "K": "X",
+            "L": "Y",
+            "M": "Z",
+            "N": "A",
+            "O": "B",
+            "P": "C",
+            "Q": "D",
+            "R": "E",
+            "S": "F",
+            "T": "G",
+            "U": "H",
+            "V": "I",
+            "W": "J",
+            "X": "K",
+            "Y": "L",
+            "Z": "M",
+        }
+
         for i in message:
-            txt += chr(ord(i)+13)
+            txt += word_dic.get(i)
+
         return txt
+
